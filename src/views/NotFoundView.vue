49e74da6f6cb3579eefac676c8c82683b7a46343
RedBlueKoi <bragin.inred@gmail.com>
2022-07-29 21:26:23 +0200

    feat: code update

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,32 +1,30 @@
 <script setup lang="ts">
   import NotFoundSVG from "@/assets/svgs/NotFound.vue"
+  import PageTitle from "@/components/PageTitle.vue"
+  import IconMdi from "@/components/IconMdi.vue"
+  import { mdiChevronLeft } from "@mdi/js"
 </script>
 
 <template>
-  <div class="flex flex-col flex-1 items-center justify-around p-8">
-    <NotFoundSVG class="max-w-2xl" />
-    <div class="flex flex-col gap-4 text-center">
-      <h1 class="text-gray-900 text-2xl">
-        Oh no! It seems that the page you were trying to look for doesn't exist!
-        <br />
-        Please try again later or click the link below.
-      </h1>
-      <router-link class="link font-medium text-emerald-900" to="/">
+  <div
+    class="flex flex-col items-center justify-center gap-4 min-h-full text-primary text-center">
+    <NotFoundSVG class="max-w-sm xl:max-w-2xl" />
+    <div class="flex flex-col gap-4">
+      <page-title>
+        <div>
+          Oh no! It seems that the page you were trying to look for doesn't
+          exist!
+        </div>
+        <div>Please try again later or click the link below.</div>
+      </page-title>
+      <router-link
+        class="link-primary link-animation self-center inline-flex items-center"
+        to="/">
+        <icon-mdi :icon="mdiChevronLeft" :size="28" />
         Return to homepage
       </router-link>
     </div>
   </div>
 </template>
 
-<style scoped lang="postcss">
-  .link {
-    &:hover {
-      @apply text-emerald-800;
-      @apply underline;
-      text-underline-offset: 2px;
-    }
-    &::before {
-      content: "\27F5";
-    }
-  }
-</style>
+<style scoped lang="postcss"></style>
