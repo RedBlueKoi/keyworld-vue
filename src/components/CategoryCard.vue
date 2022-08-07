0ea8be6888a22343ef58cbe2284a0a2cc354ffc4
John Paul Wile <botsone@gmail.com>
2022-08-06 23:19:11 -0600

    feat: added category card component

---
+++ src/components/CategoryCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="flex flex-column bg-primary shadow-md shadow-primary rounded-md overflow-hidden relative hover:-translate-y-1 hover:shadow-2xl transition-all duration-200 ease-in-out">
+    <router-link to="`categories/${getSlug(category.name)}`">
+      <img
+        :src="category.img"
+        :alt="category.name"
+        class="object-cover h-full w-full" />
+      <div
+        class="absolute top-0 left-0 pb-6 w-full h-full flex justify-center items-end bg-primary bg-opacity-40 text-white hover:opacity-0 hover:pb-12 transition-all duration-200 ease-in-out">
+        {{ category.name }}
+      </div>
+    </router-link>
+  </div>
+</template>
+
+<script lang="ts">
+  import { getSlug } from "@/utils"
+
+  export default {
+    props: {
+      category: {
+        type: Object,
+        required: true
+      }
+    }
+  }
+</script>
